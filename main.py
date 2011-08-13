0e509e568969813c602ab7bcae08349489abbc81
bubor <bubor@xl.wp.pl>
2011-08-13 10:23:05 +0200

    Build basic window template with event loop.

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
 __author__ = 'kuba'
-  
+import pygame
+
+pygame.init()
+pygame.init()
+size=[800,600]
+screen=pygame.display.set_mode(size)
+
+pygame.display.set_caption("My Game")
+done = False
+
+clock = pygame.time.Clock()
+is_balloon_growing = False
+
+while done == False:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    mouse_keys = pygame.mouse.get_pressed()
+    if mouse_keys[0] and not is_balloon_growing:
+        growing_balloon_coordinates = pygame.mouse.get_pos()
+        is_balloon_growing = True
+    if is_balloon_growing and mouse_keys[0] == 0:
+        is_balloon_growing = False
+
+    screen.fill( (255,255,255) )
+    clock.tick(20)
+    pygame.display.flip()
+
+
+pygame.quit()
